176bf82e4cc820e012a9de4af6fd70469a8f7af2
gfitz <fitzsimmonsgiuseppe@gmail.com>
2026-07-13 14:38:32 +0200

    pdf handle spinner

--- apps/documenteditor/main/app/controller/LeftMenu.js
+++ apps/documenteditor/main/app/controller/LeftMenu.js
@@ -376,12 +376,14 @@ define([
                                     pollCount++;
                                     if (!self.api.isDocumentModified() || pollCount > 7) {
                                         clearInterval(pollInterval);
-                                        Common.NotificationCenter.trigger('action:start', Asc.c_oAscAsyncActionType.Information, -1);
+                                        Common.UI.warning({
+                                            width: 400,
+                                            title: 'PDF Export',
+                                            msg: 'Your PDF is being generated. It will download shortly.',
+                                            buttons: ['ok'],
+                                            callback: function() {}
+                                        });
                                         window.location.assign('/api/files/' + fileId + '/export/pdf');
-                                        // Hide after a reasonable delay (browser handles download)
-                                        setTimeout(function() {
-                                            Common.NotificationCenter.trigger('action:end', Asc.c_oAscAsyncActionType.Information, -1);
-                                        }, 5000);
                                     }
                                 }, 100);
                                 return;
